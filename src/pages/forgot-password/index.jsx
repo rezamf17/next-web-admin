15124c6c7891d3c15af50e3344a008fb483d3ad3
reza.muhamad <reza.muhamad@pactindo.com>
2024-08-26 18:24:44 +0700

    update login register

--- src/pages/forgot-password/index.jsx
+++ src/pages/forgot-password/index.jsx
@@ -1,16 +1,12 @@
-import LockOutlined from "@ant-design/icons/LockOutlined";
-import UserOutlined from "@ant-design/icons/UserOutlined";
+import MailOutlined from "@ant-design/icons/MailOutlined";
 import { Button, Checkbox, Form, Input, Flex, Card, Row, Col } from "antd";
 import Image from 'next/image'
 import Logo from "@/styles/image/next-js-seeklogo.svg";
 import Link from "next/link";
-import { useRouter } from 'next/router';
 
 export default function LoginPage() {
-  const router = useRouter();
-  const handleLogin = (values) => {
-    console.log('Success:', values);
-    router.push('/dashboard'); // Pindah ke halaman /dashboard
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
   };
 
   return (
@@ -20,44 +16,31 @@ export default function LoginPage() {
       <Col span={10} style={{ display: "flex", justifyContent: "center" }}>
         <Card
           title="Login Web Admin"
-          style={{ width: 400, textAlign : "center" }}
-          theme="dark"
+          style={{ width: 400, textAlign : "center", height: "fit-content" }}
         >
           <Form
             name="login"
             initialValues={{ remember: true }}
             style={{ maxWidth: 360 }}
-            onFinish={handleLogin}
+            onFinish={onFinish}
           >
             <Form.Item
               name="username"
               rules={[
-                { required: true, message: "Please input your Username!" },
+                { required: true, message: "Please input your Email!" },
               ]}
             >
-              <Input prefix={<UserOutlined />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your Password!" },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined />}
-                type="password"
-                placeholder="Password"
-              />
+              <Input prefix={<MailOutlined />} placeholder="Email" />
             </Form.Item>
             <Form.Item>
               <Flex justify="end" align="center">
-                <Link href="/forgot-password">Forgot Password</Link>
+                <Link href="/">Sign in</Link>
               </Flex>
             </Form.Item>
 
             <Form.Item>
               <Button block type="primary" htmlType="submit">
-                Log in
+                Send New Password
               </Button>
             </Form.Item>
           </Form>

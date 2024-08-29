d591982edf1a8d8a3854481eac21774b43a7e23c
Reza M Fauzi <rezayuyara@gmail.com>
2024-08-29 22:01:41 +0700

    adjustment list menu

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -5,75 +5,38 @@ import {
   DesktopOutlined,
   MailOutlined,
   MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
+  UserSwitchOutlined,
+  DashboardOutlined,
 } from '@ant-design/icons';
 import { Button, Menu } from 'antd';
 const items = [
   {
-    key: '1',
-    icon: <PieChartOutlined />,
-    label: 'Option 1',
-  },
-  {
-    key: '2',
-    icon: <DesktopOutlined />,
-    label: 'Option 2',
-  },
-  {
-    key: '3',
-    icon: <ContainerOutlined />,
-    label: 'Option 3',
-  },
-  {
     key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
+    label: 'Dashboard',
+    icon: <DashboardOutlined />,
     children: [
       {
         key: '5',
-        label: 'Option 5',
+        label: 'Layanan Transaksi',
       },
       {
         key: '6',
-        label: 'Option 6',
-      },
-      {
-        key: '7',
-        label: 'Option 7',
-      },
-      {
-        key: '8',
-        label: 'Option 8',
+        label: 'Trend Transaksi',
       },
     ],
   },
   {
     key: 'sub2',
-    label: 'Navigation Two',
-    icon: <AppstoreOutlined />,
+    label: 'Maintenance User',
+    icon: <UserSwitchOutlined />,
     children: [
       {
         key: '9',
-        label: 'Option 9',
+        label: 'Manage User',
       },
       {
         key: '10',
-        label: 'Option 10',
-      },
-      {
-        key: 'sub3',
-        label: 'Submenu',
-        children: [
-          {
-            key: '11',
-            label: 'Option 11',
-          },
-          {
-            key: '12',
-            label: 'Option 12',
-          },
-        ],
+        label: 'Manage Role',
       },
     ],
   },

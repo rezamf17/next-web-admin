4b06c3b0995f276fe4176ddc5d0ef8da10d6c6c6
Reza M Fauzi <rezayuyara@gmail.com>
2024-08-29 21:56:31 +0700

    update content css

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
+import { Layout } from 'antd';
 import HeaderComponent from '@/components/HeaderComponent';
 import SiderComponent from '@/components/SiderComponent';
 
+const { Content } = Layout;
+
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed);
-  };
 
   return (
-    <div>
+    <Layout style={{ minHeight: '100vh' }}>
       <HeaderComponent />
-      <SiderComponent collapsed={collapsed} />
-    </div>
+      <Layout>
+        <SiderComponent collapsed={collapsed} />
+        <Layout style={{ marginLeft: collapsed ? 80 : 200 }}>
+          <Content className="layout-content">
+            <div className="content-wrapper">
+              {/* Main content goes here */}
+              This is the main content area.
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 };
 
